614754f449d10bedf2c4fd1c68a10648b6696b0e
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2020-10-09 18:34:21 +0200

    feat(Python): Ajout des fonctions de decoupe des lignes

--- Python/ImageEtPretraitement.py
+++ Python/ImageEtPretraitement.py
@@ -4,7 +4,7 @@ from numpy import array
 ### I - Chargement de l'image et nuance de gris
 
 image = Image.open("HelloWorld.bmp")
-image.show()
+#image.show()
 bwImage = image.convert('L') #convertion en nuance de gris
 
 ### II - Convertion en noir et blanc (0/1)
@@ -15,12 +15,8 @@ la plus élevé et la plus basse. On creer ensuite une liste de 0/1,
 """
 
 greyArray = array(bwImage)
-print(greyArray)
-    
-#Considerant que l'image n'est que du texte unicolore
-#sur un fond unicolore, on sépare le texte du fond en cherchant
-#les valeurs min et max de la liste.
 
+"""
 def findextrem (mat):
     maxi = mat[0][0]
     mini = mat[0][0]
@@ -48,6 +44,78 @@ def buildbinlist (mat):
             else:
                 binList.append(1)
     return binList
+"""
 
 
-print(builbinlist(greyArray))
+### III - Decoupe
+# Decoupe des lignes
+def lineDetection(M):
+    """
+    Cette fonction prend une matrice M et realise une projection horizontale des 1 sur la gauche
+    Elle sépare la matrice en sous matrice qui correspondent aux lignes
+    Ne pas utiliser si l'image/matrice n'a pas été traité
+    M : Matrice binarizé et traité
+    return : projectionM : matrice correspondant au projeté horizontale de M
+    """
+    lM = len(M)
+    cM = len(M[0])
+
+    #Construction de la matrice projection à la bonne taille
+    listOfZero = [0 for i in range(cM)]
+    projectionM = []
+    for i in range(lM):
+        L = listOfZero[:]
+        projectionM.append(L)
+
+    #projection horizontale
+    for i in range(lM):
+        indexOfOne = 0
+        for j in range(cM):
+            if M[i][j] == 1:
+                projectionM[i][indexOfOne] = 1
+                indexOfOne += 1
+
+    return projectionM
+
+
+Mtest = [[1,0,1],[0,0,1],[1,0,0]]
+print(lineDetection(Mtest))
+
+
+def lineSlice(M):
+    """
+    M : matrice ayant subis le projeté horizontale
+    return : liste de matrices correspondant aux lignes d'ecriture de l'image
+    """
+    lM = len(M)
+    cM = len(M[0])
+    matrixOfLines = []
+    matrixOfPxl = []
+    isFullOfZero = True
+
+    for i in range(lM):
+        j = 0
+        isFullOfZero = True
+
+        #si la liste est plein de 0 alors c'est une interligne
+        while isFullOfZero and j<cM:
+            if M[i][j] == 1:
+                isFullOfZero = False
+            j += 1
+        
+        #si c'est plein on ajoute a la matrixOfPxl pour recreer la rangé de pixel
+        if not isFullOfZero:
+            matrixOfPxl.append(M[i])
+        else: #une interligne donc notre ligne est complete on peut tout ajouter à la matrice de lignes
+            if matrixOfPxl != []: #si la lgine de pixels d'avant n'était pas deja une interligne
+                matrixOfLines.append(matrixOfPxl)
+                matrixOfPxl = []
+
+    return matrixOfLines
+
+
+Mtest = Mtest = [[0,0,1],[0,0,1],[0,0,0],[0,0,0],[1,0,0],[1,0,0],[0,0,0],[1,1,1],[1,1,1]]
+print(lineSlice(Mtest))
+
+
+# Todo: Découpe des char (meme technique mais en projeté verticale)
