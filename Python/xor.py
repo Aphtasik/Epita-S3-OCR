85f3ab2d0ac39de23ea3e4350c1689de523e0fbd
mathis guilbaud <mathis.guilbaud@epita.fr>
2020-10-20 12:43:26 +0200

    xor gate in python

--- Python/xor.py
+++ Python/xor.py
@@ -1,86 +1,47 @@
-import random
 import numpy as np
 
-def sigmoid(x): #fonction sigmoide
+inputs = np.array([[0, 0],
+                    [0, 1],
+                    [1, 0],
+                    [1, 1]])
+
+outputs = np.array([0,1,1,0])
+outputs.reshape((4,1))
+
+weights = np.array([[0.1],[0.2]])
+bias = 0.3
+
+def sigmoid(x,deriv=False):
+    if deriv:
+        return sigmoid(x) * (1- sigmoid(x))
     return 1/(1+np.exp(-x))
 
-def sigmoid_prime(x): #derivee de la fonction sigmoide
-    return sigmoid(x)*(1-sigmoid(x))
+for epochs in range(10000):
+    input_arr = inputs
+    weighted_sum = np.dot(input_arr,weights) + bias
+    first_output = sigmoid(weighted_sum)
 
+    error = first_output - outputs
+    total_error = np.square(np.subtract(first_output,outputs)).mean()
+    #print(total_error)
 
-class Network:
-    def __init__(self,sizes): # sizes <=> liste dont la longeur = nb layer et size[i] = nb neuron sur cette layer
-        self.num_layer = len(sizes)
-        self.sizes = sizes
-        self.biases = [np.random.randn(y,1) for y in sizes[1:]]
-        self.weights = [np.randn(y,x) for x,y in zip(sizes[:-1],sizes[1:])]
+    first_der = error
+    second_der = sigmoid(first_output,True)
+    derivative = first_der * second_der
+    t_input = inputs.T
+    final_derivative = np.dot(t_input,derivative)
 
-    def feedforward(self,a): # renvoie le resultat du réseau, a est l'input
-        for b,w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w,a)+b)
-        return a
+    weights = weights - 0.05 * final_derivative
 
-    def training(self,training_data,epochs,mini_batch_size,eta,test_data=None):
-        """
-        training data: liste de tuple (x,y) x = input data y = resultats attendu
-        epoch: nombre de cycle d'entrainement
-        test_data: mettre a True pour avoir un display des résultats
-        eta : learning rate
-        """
-        if test_data:
-            n_test = len(test_data)
-        n = len(training_data)
-        for i in xrange(epochs):
-            random.shuffle(training_data) #melange des input_data
-            mini_batches = [training_data[k:k + mini_batch_size] for k in xrange(0,n,mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch,eta)
-            if test_data:
-                print "Epoch {0}: {1} / {2}".format(i,self.evaluate(test_data),n_test)
-            else:
-                print "Epoch {0} complete".format(i)
+    for i in derivative:
+        bias = bias - 0.05 * i
 
+print(weights)
+print(bias)
 
-    def update_mini_batch(self,mini_batch,eta): # change les biais et poids du réseau a l'aide de backpropagation
-       mat_b = [np.zeros(b.shape) for b in self.biases]
-       mat_w = [np.zeros(w.shape) for w in self.weights]
-       for x,y in mini_batch:
-           delta_mat_b, delta_mat_w = self.backprop(x,y)
-           mat_b = [nb+dnb for nb, dnb in zip(mat_b, dalta_mat_b)]
-           mat_w = [nw+dnw for nw, dnw in zip(mat_w, delta_mat_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw for w,nw in zip(self.weights,mat_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb for b,nb in zip(self.biases,mat_b)]
+prediction = np.array([0,1])
 
-    def backprop(self,x,y):
-        """
-        retourne un tuple représentant l'appication de la descente de gradient
-        pour la fonction d'efficacité du reseau
-        """
-        mat_b = [np.zeros(b.shape) for b in self.biases]
-        mat_w = [np.zeros(w.shape) for w in self.weights]
-        activation = x
-        activations = [x] #enregistre toutes les activation layer par layer
-        zs = [] #liste qui enregistre tous les vecteur de correction pour le réseau
-        for b,w in zip(self.biases,self.weights):
-            z = np.dot(w,activation) + b
-            zs.append(z)
-            activation = sigmoid(z)
-            activations.append(activation)
-            delta = self.cost_derivative(activations[-1],y) * sigmoid_prime(zs[-1])
-            mat_b[-1] = delta
-            mat_w[-1] = np.dot(delta,activations[-2].transpose())
-            for layer_n in xrange(2,self.num_layers):
-                z = zs[-layer_n]
-                sp = sigmoid_prime(z)
-                delta = np.dot(self.weights[-layer_n+1].transpose(),delta) * sp
-                mat_b[layer_n]= delta
-                mat_w[-layer_n] = np.dot(delta,activation[-layer_n-1].transpose())
-            return (mat_b,mat_w)
+result = np.dot(prediction,weights) + bias
+res = sigmoid(result)
 
-    def evaluate(self,test_data):
-        test_results = [(np.argmax(self.feedforward(x)),y) for (x,y) in test_data]
-        return sum(int(x==y) for (x,y) intest_results)
-
-    def cost_derivative(self,output_activation, y):
-        return (output_activation-y)
- 
+print(res)
