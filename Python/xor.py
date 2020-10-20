8322f56aea954accc5984457b329b53a81717814
mathis guilbaud <mathis.guilbaud@epita.fr>
2020-10-20 14:34:41 +0200

    fix: affichage de plusieur valeur de retour, bug: xor ne fonctionne pas pour des raisons mystiques

--- Python/xor.py
+++ Python/xor.py
@@ -1,45 +1,51 @@
 import numpy as np
 
-inputs = np.array([[0, 0],
-                    [0, 1],
-                    [1, 0],
-                    [1, 1]])
+inputs = np.array([[0, 1],
+                    [0, 0],
+                    [1, 1],
+                    [1, 0]])
 
-outputs = np.array([0,1,1,0])
-outputs.reshape((4,1))
 
+outputs = np.array([1,0,0,1])
+outputs = outputs.reshape(4,1)
+print(outputs.shape)
 weights = np.array([[0.1],[0.2]])
 bias = 0.3
 
 def sigmoid(x,deriv=False):
     if deriv:
-        return sigmoid(x) * (1- sigmoid(x))
+        return (1/(1+np.exp(-x))) * (1- (1/(1+np.exp(-x))))
     return 1/(1+np.exp(-x))
-
 for epochs in range(10000):
     input_arr = inputs
     weighted_sum = np.dot(input_arr,weights) + bias
     first_output = sigmoid(weighted_sum)
-
+    
     error = first_output - outputs
     total_error = np.square(np.subtract(first_output,outputs)).mean()
     #print(total_error)
+    #print("weighted_sum")
+    #print(weighted_sum)
+    #print("first_output")
+    #print(first_output)
+    #print("error")
+    #print(error)
 
     first_der = error
     second_der = sigmoid(first_output,True)
     derivative = first_der * second_der
     t_input = inputs.T
     final_derivative = np.dot(t_input,derivative)
-
+    #print("final_derivative")
+    #print(final_derivative)
     weights = weights - 0.05 * final_derivative
-
+    #print("weights")
+    #print(weights)
     for i in derivative:
         bias = bias - 0.05 * i
+#print(bias)
 
-print(weights)
-print(bias)
-
-prediction = np.array([0,1])
+prediction = np.array([1,0])
 
 result = np.dot(prediction,weights) + bias
 res = sigmoid(result)
