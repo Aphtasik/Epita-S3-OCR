1a2ab0511e54adc98fe6d60766aee8a0a278e404
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2020-11-17 11:05:14 +0100

    feat(Tools): Ajout des fct dans le .h

--- C/Tools.h
+++ C/Tools.h
@@ -13,6 +13,15 @@ struct Matrix
 };
 
 struct Matrix CreateMatrix(int rows, int columns);
+
 int IsInMatrix(struct Matrix matrix, int x, int y);
+
+double MovePointerInMatrix(struct Matrix matrix, int x, int y);
+void ChangeEltInMatrix(struct Matrix matrix, int x, int y, float val);
+
+void PrintEltIntMatrix(struct Matrix matrix, int x, int y);
+void PrintMatrix(struct Matrix matrix);
+
+void InitialiseMatrixWithZero(struct Matrix matrix);
 
 #endif
