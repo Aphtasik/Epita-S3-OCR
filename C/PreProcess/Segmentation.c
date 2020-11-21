1d5d4a8b51e4328227ecf1608055bb19996fb9cf
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2020-11-21 11:21:57 +0100

    fix(Segmentation): ProjH et V + LigneSize

--- C/PreProcess/Segmentation.c
+++ C/PreProcess/Segmentation.c
@@ -47,7 +47,7 @@ void VerticalProjection(struct Matrix matrix, int* pList)
         sum = 0;
         for(int j = 0; j < matrix.rows; j++)
         {
-            if(MovePointerInMatrix(matrix, i, j) == 1.0)
+            if(MovePointerInMatrix(matrix, j, i) == 1.0)
             {
                 sum+=1;
             }
@@ -64,35 +64,70 @@ int LigneSize(int *pProjH, int lenList)
     int isFound = 0;
     int onSpree = 0;
     int i = 0;
+    int *p;
+    p = pProjH;
 
     while(i < lenList && isFound == 0)
     {
-        if(onSpree == 0 && *pProjH != 0)
+        if(onSpree == 0 && *p != 0)
         {
             onSpree = 1;
             res++;
         }
-        else if (onSpree == 1 && *pProjH != 0)
-        {
+        else if (onSpree == 1 && *p != 0)
+        {   
             res++;
         }
-        else if (onSpree == 1 && *pProjH == 0)
+        else if (onSpree == 1 && *p == 0)
         {
             isFound = 1;
         }
         i++;
+        p++;
     }
     return res;
 }
 
 
+
+
+
 int main()
 {
-    struct Matrix matrix = CreateMatrix(2,2);
+    struct Matrix matrix = CreateMatrix(2,3);
     ChangeEltInMatrix(matrix, 0,1,1);
+    ChangeEltInMatrix(matrix, 1,2,1);
+    ChangeEltInMatrix(matrix, 1,1,1);
     PrintMatrix(matrix);
     printf("\n");
 
     int *pProjH = malloc(sizeof(int)*matrix.rows);
-    int *pProjH = malloc(sizeof(int)*matrix.columns);
+    int *pProjV = malloc(sizeof(int)*matrix.columns);
+
+    int *p;
+    //TEST HorizontalProj
+    HorizontalProjection(matrix, pProjH);
+    p = pProjH;
+    for(int i = 0 ; i < matrix.rows;i++)
+    {
+        printf("%i\n", *p);
+        p++;
+    }
+    printf("\n");
+
+    //TEST VerticalProj
+    VerticalProjection(matrix, pProjV);
+    p = pProjV;
+    for(int i = 0 ; i < matrix.columns;i++)
+    {
+        printf("%i\n", *p);
+        p++;
+    }
+    printf("\n");
+
+    //TEST LigneSize
+    printf("Ligne size = %i\n", LigneSize(pProjH, matrix.rows));
+
+
+
 }
