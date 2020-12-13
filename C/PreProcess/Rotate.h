4cfbed343c775ef7239629dabc71e678616c1b40
Alexiserba <55802419+Alexiserba@users.noreply.github.com>
2020-12-14 00:18:29 +0100

    Add files via upload

---
+++ C/PreProcess/Rotate.h
@@ -0,0 +1,20 @@
+#ifndef ROTATE_H_
+#define ROTATE_H_
+
+#include <err.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include <stdlib.h>
+#include <SDL.h>
+#include "pixel_operations.h"
+#include <math.h>
+
+SDL_Surface* Rotation(SDL_Surface* origine, float angle);
+void HorizontalProjection(SDL_Surface* image, int* pList);
+float Moyenne(SDL_Surface* image, int* pList);
+float Variance(SDL_Surface* image, int* pList);
+int CalculAngle (SDL_Surface* image);
+SDL_Surface* VraiRotation(SDL_Surface* image);
+
+
+#endif
